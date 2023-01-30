c72e9272e7db773fb1703d65785f31a4ba92735d
Marcos Vinicius <contato.shopvmax@gmail.com>
2023-01-30 15:19:45 -0300

    feat : finalizando component de lista

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,30 @@
-import { Container } from './styles'
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { GroupCard } from '@components/GrupCard';
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+import { Container } from "./styles";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GrupCard";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da rocket",
+    "Timeware",
+  ]);
+
   return (
     <Container>
-      <Header/>
+      <Header />
 
-      <Highlight 
-      title="Turmas"
-      subtitle='Jogue com a sua turma'
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        //declarando uma chave unica para renderizar a lista
+        keyExtractor={(item) => item}
+        //declarando a renderização da lista
+        renderItem={({ item }) => <GroupCard title={item} />}
       />
-      <GroupCard title='Galera do ignite' />
     </Container>
   );
 }
-
